945e33e917efa02aad84de07f090587f51be6719
Michael Bradley <mbradleyis@gmail.com>
2016-03-05 13:57:57 -0800

    removed extraneous packages

--- webpack.config.js
+++ webpack.config.js
@@ -39,24 +39,6 @@ var config = {
       { include: path.join(__dirname, "src"),
         test: /\.jsx?$/,
         loaders: ["babel"]
-      },
-      { test: /\.css?$/,
-        loader: "style-loader!css-loader"
-      },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file"
-      },
-      { test: /\.(woff|woff2)$/,
-        loader:"url?prefix=font/&limit=5000"
-      },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=application/octet-stream"
-      },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=image/svg+xml"
-      },
-      { test: /\.png$/,
-        loader: "url-loader?mimetype=image/png"
       }
     ]
   }
